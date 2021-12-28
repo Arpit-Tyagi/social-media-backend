44abf7117fb9f81a4fe8b2fe95927e1aa8a6f816
Arpit-Tyagi <arpittyagi36@gmail.com>
2021-12-28 18:55:31 +0530

    Second commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpException, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schema/user.schema';
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Post('/register')
+  create(@Body() createUserDto: User) {
+
+      return  this.usersService.create(createUserDto);
+     
+  }
+
+  @Post('/userLogin')
+  loginUser(@Body() createUserDto:any) {
+    return this.usersService.userLogin(createUserDto);
   }
 
   @Get()
